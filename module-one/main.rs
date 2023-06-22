aa2d5e0bd970c691fddabfa0e3de5e1e3b44442c
web3nomad.eth <me@web3nomad.eth>
2023-06-23 02:29:50 +0800

    play rust

--- module-one/main.rs
+++ module-one/main.rs
@@ -1,3 +1,68 @@
-fn main() {
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+fn test1() {
+    const RUST: &str = "rust";
+    const WEIGHT: u64 = 100;
+
     println!("Hello Rust");
+    println!("{}", RUST);
+    println!("{}", WEIGHT);
+
+    let name = "2001";
+    println!("{}", name);
+
+    let name_to_i32 = match name.parse::<i32>() {
+        Ok(n) => n,
+        Err(_) => panic!("Not a number"),
+    };
+    println!("{}", name_to_i32);
+    let name_to_i32_alt = name.parse::<i32>().unwrap();
+    println!("{}", name_to_i32_alt);
+}
+
+fn test2() {
+    let t = (1, "hello", true, 9.1);
+    println!("{:?}", t);
+    println!("{}", t.1);
+
+    let arr: [i32; 6] = [1, 2, 3, 4, 5, 0];
+    println!("{:?}", arr);
+    println!("{}", arr[1]);
+
+    let s1 = "Hello Rust";
+
+    let s2 = String::new();         // 空字符串
+    let s2 = "Hello Rust".to_string();
+    let s2 = s1.to_string();
+    let s2: String = "Hello Rust".into();
+    let s2: String = s1.into();
+    let s2 = String::from("Hello Rust");
+
+    let s3 = &s2[0..5];
+    println!("{}", s3);
+}
+
+fn test3() {
+    let mut v: Vec<i32> = Vec::new();
+    println!("{:?}", v);
+    v.push(10);
+
+    let mut vec1 = Vec::new();
+    let mut vec2 = vec![];
+
+    vec1.push("rust");
+    // vec1.push(1);
+    vec2.push(true);
+
+    use std::collections::HashMap;
+    let mut map1 = HashMap::new();
+    map1.insert(String::from("rust"), 1);
+    println!("{:?}", map1);
+}
+
+fn main() {
+    // test1();
+    // test2();
+    test3();
 }
