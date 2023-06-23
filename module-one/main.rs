9869bd6dc009fd9a04e8ea4090827e594d894aa7
web3nomad.eth <me@web3nomad.eth>
2023-06-23 11:29:17 +0800

    play rust 2

--- module-one/main.rs
+++ module-one/main.rs
@@ -59,10 +59,82 @@ fn test3() {
     let mut map1 = HashMap::new();
     map1.insert(String::from("rust"), 1);
     println!("{:?}", map1);
+    let v = *(map1.get("rust").unwrap());
+    let v = map1.get("rust").unwrap();
+    println!("{}", v);
+}
+
+fn test4() {
+    let arr1: [i32; 3] = [1, 2, 3];
+    let arr2 = arr1;
+    let arr3 = arr1;
+
+    println!("arr1 addr {:p}", &arr1);
+    println!("arr2 addr {:p}", &arr2);
+    println!("arr3 addr {:p}", &arr3);
+    // 三个地址是不一样的，但是值一样，发生了值拷贝
+
+    fn return_string(x: &String) -> String {
+        // x
+        let s = String::from("Rust s");
+        s
+    }
+
+    let s_p = return_string(&String::from("Rust a"));
+    println!("{}", s_p);
+
+    struct Foo {
+        x: i32,
+        y: (i32, bool),
+        z: String,
+    }
+
+    // 'a:'b,表示'a不短于'b, 只能返回x, 因为最后要返回的 Foo 的生命周期是 'a
+    fn bar2<'a: 'b, 'b>(x: &'a Foo, y: &'b Foo) -> &'a Foo {
+        x
+        // y
+    }
+}
+
+fn test_trait() {
+    trait Hacker {
+        fn speak(&self) -> &str {
+            "I'm hacking!"
+        }
+    }
+    struct Person {}
+    struct AnotherPerson {}
+    impl Hacker for Person {}
+    fn say_hi_to_hacker(hacker: &dyn Hacker) {
+        println!("Hi, hacker! \n{}", hacker.speak());
+    }
+    let p = Person {};
+    say_hi_to_hacker(&p);
+
+
+    trait A {
+        fn take_ownership(self);
+    }
+    
+    #[derive(Debug)]
+    struct X;
+
+    impl A for X {
+        fn take_ownership(self) {
+            // do nothing
+        }
+    }
+    let x = X {};
+    println!("before take_ownership: {:?}", x);
+    x.take_ownership();
+    // println!("after take_ownership{:?}", x);  // 会失败，已经 take_ownership 了
+
 }
 
 fn main() {
     // test1();
     // test2();
-    test3();
+    // test3();
+    // test4();
+    test_trait();
 }
