c202b9ba7d688f3a4bdd37834335190223fdd846
web3nomad.eth <me@web3nomad.eth>
2023-06-30 02:26:59 +0800

    request to openai api

--- ai-bots/src/main.rs
+++ ai-bots/src/main.rs
@@ -1,9 +1,26 @@
 extern crate dotenv;
 
+use reqwest;
 use dotenv::dotenv;
 use std::env;
+use tokio;
 
-fn main() {
+async fn get_body() -> Result<String, reqwest::Error> {
+    // call openai api using gpt-3.5-turbo model, with native request crate
+    // let url = "https://api.openai.com/v1/engines/davinci-codex/completions";
+
+    let body = reqwest::get("https://dxd1234.heidianer.com/.ping")
+        .await?
+        .text()
+        .await?;
+
+    println!("body = {:?}", body);
+
+    Ok(body)
+}
+
+#[tokio::main]
+async fn main() {
     dotenv().ok();
 
     for (key, value) in env::vars() {
@@ -11,4 +28,20 @@ fn main() {
             println!("{}: {}", key, value);
         }
     }
+
+    get_body().await.unwrap();
+
+    let mut a = 1;
+
+    let mut c = || {
+        a += 1;
+        println!("a: {}", a);
+        // println!("a");
+    };
+
+    c();
+
+    // call_fn_once(c); // 实现了 FnOnce trait
+    // call_fn(c); // 实现了Fn trait，FnMut trait,FnOnce trait,后面两种trait都是通过继承实现的
+    // call_fn_mut(c); // 实现了FnMut trait,FnOnce trait
 }
