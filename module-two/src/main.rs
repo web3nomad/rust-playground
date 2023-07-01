845a4d16d65a48eb4d7ff5b8ee5954146f90564d
web3nomad.eth <me@web3nomad.eth>
2023-07-02 06:04:54 +0800

    play with ? and unwrap

--- module-two/src/main.rs
+++ module-two/src/main.rs
@@ -1,5 +1,5 @@
 
-fn main() {
+fn test1() {
     let mut a = 1;
 
     let mut c = || {
@@ -9,4 +9,29 @@ fn main() {
     };
 
     c();
+
+    let _x = [1,2,3,4,5];
+    let y = String::from("hello");
+    {
+        let z = &(*y);
+        println!("y: {:?}", z);
+    }
+    let zz = y;
+    println!("y: {:?}", zz);
+}
+
+fn test2() {
+    fn f() -> Option<i32> {
+        let a = Some(12);
+        let b: Option<i32> = None;
+        let _c = b?;  // 这里会直接 return None;
+        a
+    }
+    let c = f();
+    println!("c: {:?}", c);
+}
+
+fn main() {
+    test1();
+    test2();
 }
