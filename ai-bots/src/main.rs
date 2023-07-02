70e5f573edbb78b04ac76c915b149482d3e045f8
web3nomad.eth <me@web3nomad.eth>
2023-07-03 00:37:43 +0800

    connect openai api

--- ai-bots/src/main.rs
+++ ai-bots/src/main.rs
@@ -1,14 +1,12 @@
-extern crate dotenv;
+// extern crate dotenv;
 
-use reqwest;
 use dotenv::dotenv;
 use std::env;
+use reqwest;
+use serde_json::json;
 use tokio;
 
-async fn get_body() -> Result<String, reqwest::Error> {
-    // call openai api using gpt-3.5-turbo model, with native request crate
-    // let url = "https://api.openai.com/v1/engines/davinci-codex/completions";
-
+async fn _get_body_simple() -> Result<String, reqwest::Error> {
     let body = reqwest::get("https://dxd1234.heidianer.com/.ping")
         .await?
         .text()
@@ -19,15 +17,48 @@ async fn get_body() -> Result<String, reqwest::Error> {
     Ok(body)
 }
 
+async fn request_openai(openai_token: &str) -> Result<String, reqwest::Error> {
+    // call openai's chat api using gpt-3.5-turbo model, with reqwest crate
+    let body = reqwest::Client::new()
+        .post("https://api.openai.com/v1/chat/completions")
+        .bearer_auth(openai_token)
+        .json(&json!({
+            "model": "gpt-3.5-turbo",
+            "messages": [
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": "Hello!"}
+            ]
+        }))
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    // println!("body = {:?}\n", body);
+
+    let json_data: serde_json::Value = serde_json::from_str(&body).unwrap();
+    // println!("json_data = {:?}\n", json_data);
+    // println!("json_data = {:?}\n", json_data["choices"][0]["message"]["content"]);
+    let message_value = &(json_data["choices"][0]["message"]["content"]);
+    let response = message_value.as_str().unwrap().to_string();
+    // let response = json_data["choices"][0]["message"]["content"].as_str().unwrap().to_string();
+
+    Ok(response)
+}
+
 #[tokio::main]
 async fn main() {
     dotenv().ok();
-
     for (key, value) in env::vars() {
         if key == "OPENAI_API_KEY" {
             println!("{}: {}", key, value);
         }
     }
 
-    get_body().await.unwrap();
+    // let res = _get_body_simple().await;
+    // println!("res = {:?}", res.unwrap());
+
+    let openai_token = env::var("OPENAI_API_KEY").unwrap();
+    let res = request_openai(&openai_token).await;
+    println!("res = {:?}", res.unwrap());
 }
