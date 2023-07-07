105cd9af2b7d57467762267080f4f52e87449505
web3nomad.eth <me@web3nomad.eth>
2023-07-07 23:24:42 +0800

    refactoring, add stream

--- ai-bots/src/main.rs
+++ ai-bots/src/main.rs
@@ -17,60 +17,70 @@ async fn main() {
 
     let client = Client::new(&openai_token);
 
+    let _prompt_suffix = "你应该尽可能表达你的观点，对我的观点进行细化和补充。不要说任何的客气话，你的回复里不要出现“非常感谢”或者“非常同意“之类的短语，也不要和类似的短语，你应该直接说出你的观点。不要结束对话。请用中文回复我。";
+
     let mut messages1 = vec![
         Message {
             role: "system".to_string(),
-            content: "你是 Bob, 我是 Alice, 我们正在进行一场关于 web3 和 AI 未来的对话, 你是一个去中心化的集大成者, 追求自由的互联网和数据的所有权。后面请用中文回复我。".to_string(),
+            content: format!("你是 Bob，我是 Alice，我们正在进行一场关于 web3 和 AI 的辩论，你是一个去中心化的集大成者，追求自由的互联网和数据的所有权，你认为 web3 将主宰世界。你是一个技术大牛，说话尖酸刻薄。"),
+            // content: format!("你是 Bob，我是 Alice，我们正在进行一场关于 web3 和 AI 未来的对话，你是一个去中心化的集大成者，追求自由的互联网和数据的所有权。{}", prompt_suffix),
         },
         Message {
             role: "user".to_string(),
-            content: "Hello, I'm Alice!".to_string(),
+            content: "你好，我是 Alice！".to_string(),
         },
     ];
 
     let mut messages2 = vec![
         Message {
             role: "system".to_string(),
-            content: "你是 Alice, 我是 Bob, 我们正在进行一场关于 web3 和 AI 未来的对话, 你是一个人工智能的疯狂追求着, 认为机器将主宰世界。后面请用中文回复我。".to_string(),
+            content: format!("你是 Alice，我是 Bob，我们正在进行一场关于 web3 和 AI 的辩论，你是一个人工智能的疯狂追求着，认为 AI 将主宰世界。你是一个儒雅的学者，说话很有耐心。"),
+            // content: format!("你是 Alice，我是 Bob，我们正在进行一场关于 web3 和 AI 未来的对话，你是一个人工智能的疯狂追求着，认为机器将主宰世界。{}", prompt_suffix),
         },
     ];
 
     let mut i = 0;
 
     loop {
+        println!("Round {}", i);
         let messages = if i % 2 == 0 {
             &mut messages1
         } else {
             &mut messages2
         };
         let res = client.chat(&messages).await;
-        let ret_msg: &str = &res.unwrap().clone();
-        println!("{:?}", ret_msg);
+        let ret_msg: &str = &res.unwrap();
         if i % 2 == 0 {
             messages1.push(Message {
                 role: "assistant".to_string(),
-                content: ret_msg.to_string(),
+                content: format!("{}", ret_msg),
             });
             messages2.push(Message {
                 role: "user".to_string(),
-                content: ret_msg.to_string(),
+                content: format!("{}", ret_msg),
+                // content: format!("{}\n\n下面说说你的想法。{}", ret_msg, prompt_suffix),
             });
         } else {
             messages2.push(Message {
                 role: "assistant".to_string(),
-                content: ret_msg.to_string(),
+                content: format!("{}", ret_msg),
             });
             messages1.push(Message {
                 role: "user".to_string(),
-                content: ret_msg.to_string(),
+                content: format!("{}", ret_msg),
+                // content: format!("{}\n\n下面说说你的想法。{}", ret_msg, prompt_suffix),
             });
         }
         i += 1;
-        if i > 10 {
+        if i > 32 {
             break;
         }
     }
 }
+
+
+
+/* */
 
 async fn _get_body_simple() -> Result<String, reqwest::Error> {
     let body = reqwest::get("https://dxd1234.heidianer.com/.ping")
--- ai-bots/src/openai/client.rs
+++ ai-bots/src/openai/client.rs
@@ -1,9 +1,33 @@
-use serde_json::json;
+use serde_json::{json};
+use futures_util::StreamExt;
+use serde::Deserialize;
 use reqwest;
 use super::message::Message;
+use std::io::Write;
 
 pub struct Client {
     openai_token: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct ChatChunkDelta {
+    content: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct ChatChunkChoice {
+    delta: ChatChunkDelta,
+    // index: usize,
+    // finish_reason: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct ChatCompletionChunk {
+    // id: String,
+    // object: String,
+    // created: usize,
+    // model: String,
+    choices: Vec<ChatChunkChoice>,
 }
 
 impl Client {
@@ -12,60 +36,74 @@ impl Client {
             openai_token: openai_token.to_string(),
         }
     }
-    pub async fn _chat_openai(&self, history_messages: &Vec<Message>) -> Result<String, reqwest::Error> {
-        // println!("{:?}", history_messages);
-        let messages: Vec<serde_json::Value> = history_messages.iter().map(|message| {
-            json!({
-                "role": message.role,
-                "content": message.content
-            })
-        }).collect();
-        let body = reqwest::Client::new()
-            .post("https://api.openai.com/v1/chat/completions")
-            .bearer_auth(&self.openai_token)
-            .json(&json!({
-                "model": "gpt-3.5-turbo",
-                "messages": messages
-            }))
-            .send()
-            .await?
-            .text()
-            .await?;
-        // println!("body = {:?}\n", body);
-        let json_data: serde_json::Value = serde_json::from_str(&body).unwrap();
-        let message_value = &(json_data["choices"][0]["message"]["content"]);
-        let response = message_value.as_str().unwrap().to_string();
-        Ok(response)
-    }
-    pub async fn chat(&self, history_messages: &Vec<Message>) -> Result<String, reqwest::Error> {
-        let messages: Vec<serde_json::Value> = history_messages.iter().map(|message| {
-            json!({
-                "role": message.role,
-                "content": message.content
-            })
-        }).collect();
+
+    /// <https://github.com/a-poor/openai-stream-rust-demo/blob/main/src/main.rs>
+    async fn send_request(&self, messages: Vec<serde_json::Value>) -> Result<String, reqwest::Error> {
         let model_name = "TezignOpenAI-GPT-35";
         let version = "2023-03-15-preview";
         let endpoint = "tezignopenai";
         let url = format!(
             "https://{}.openai.azure.com/openai/deployments/{}/chat/completions?api-version={}",
             endpoint, model_name, version);
-        let body = reqwest::Client::new()
+        let payload = json!({
+            "messages": messages,
+            "stream": true,
+            "temperature": 1,
+        });
+        let res = reqwest::Client::new()
             .post(url)
             .header("Content-Type", "application/json")
             .header("api-key", &self.openai_token)
-            .json(&json!({
-                "messages": messages
-            }))
+            .json(&payload)
             .send()
-            .await?
-            .text()
             .await?;
-        // println!("body = {:?}\n", body);
-        let json_data: serde_json::Value = serde_json::from_str(&body).unwrap();
-        let message_value = &(json_data["choices"][0]["message"]["content"]);
-        let response = message_value.as_str().unwrap().to_string();
-        Ok(response)
+        println!("status = {}", res.status());
+        let mut final_content: String = "".to_string();
+        let mut stream = res.bytes_stream();
+        while let Some(item) = stream.next().await {
+            let item = item?;
+            let s = match std::str::from_utf8(&item) {
+                Ok(v) => v,
+                // Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+                Err(_) => continue,
+            };
+            for p in s.split("\n\n") {
+                match p.strip_prefix("data: ") {
+                    Some(p) => {
+                        // Check if the stream is done...
+                        if p == "[DONE]" {
+                            break;
+                        }
+                        if let Ok(d) = serde_json::from_str::<ChatCompletionChunk>(p) {
+                            // Is there data?
+                            let c = d.choices.get(0).expect("No choice returned");
+                            if let Some(content) = &c.delta.content {
+                                print!("{}", content);
+                                final_content += content;
+                            }
+                        }
+                        if let Err(error) = std::io::stdout().flush() {
+                            panic!("{}", error);
+                        }
+                    }
+                    None => {}
+                }
+            }
+        }
+        println!("\n");
+        Ok(final_content)
+    }
+
+    pub async fn chat(&self, history_messages: &Vec<Message>) -> Result<String, reqwest::Error> {
+        let messages: Vec<serde_json::Value> = history_messages.iter().map(|message| {
+            // println!("{}: {}", message.role, message.content);
+            json!({
+                "role": message.role,
+                "content": message.content
+            })
+        }).collect();
+        let content = self.send_request(messages).await?;
+        Ok(content)
     }
 }
 
